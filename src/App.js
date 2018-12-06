638414efd0a2e808dcb4afbfd52be9685fece1f1
peterdyer7 <peter_dyer@hotmail.com>
2018-12-06 10:42:59 -0500

    Day3 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { createCache, createResource } from 'react-cache';
+
+const cache = createCache();
+const PokemonCollectionResource = createResource(async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/');
+  return res.json();
+});
+
+function PokemonListItem({ className, component: Component = 'li', ...props }) {
+  return (
+    <Component
+      className={['pokemon-list-item', className].join(' ')}
+      {...props}
+    />
+  );
+}
+
+function PokemonList() {
+  return (
+    <ul>
+      {PokemonCollectionResource.read(cache).results.map((item) => (
+        <PokemonListItem key={item.name}>{item.name}</PokemonListItem>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
   return (
@@ -9,6 +35,9 @@ function App() {
         </span>
         : Day 3
       </h1>
+      <Suspense fallback={<div>...loading</div>}>
+        <PokemonList />
+      </Suspense>
     </div>
   );
 }
